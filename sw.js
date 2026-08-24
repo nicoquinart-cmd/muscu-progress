a97a6032a0e0c552b70a96b1f9bfefc706a7fdcb
nicoquinart-cmd <nicoquinart@gmail.com>
2026-08-24 21:09:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-const CACHE="muscu-progress-v2-1";
-const ASSETS=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));});
+const CACHE="muscu-progress-v2-1-2";
+const STATIC=["./manifest.json"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  const req=event.request;
+  if(req.mode==="navigate"){
+    event.respondWith(fetch(req).catch(()=>caches.match("./")));
+    return;
+  }
+  event.respondWith(
+    fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(cache=>cache.put(req,copy));
+      return res;
+    }).catch(()=>caches.match(req))
+  );
+});
