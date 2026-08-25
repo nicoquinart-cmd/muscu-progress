d1fea504a8ec5cf5235b1fc24fe0f5de0da02b71
nicoquinart-cmd <nicoquinart@gmail.com>
2026-08-25 23:50:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="fitpilot-v3-0";
-const STATIC=["./manifest.json","./fitpilot-icon.svg"];
+const CACHE="fitpilot-v3-0-1";
+const STATIC=["./manifest.json","./fitpilot-icon.svg","./fitpilot-icon-192.png","./fitpilot-icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(e.request.mode==="navigate"){e.respondWith(fetch(e.request));return;}e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
