9445d842186aa0e1029683ad1c54df7486da5c8d
nicoquinart-cmd <nicoquinart@gmail.com>
2026-08-25 23:31:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="muscu-progress-v2-6";
+const CACHE="muscu-progress-v2-7";
 const STATIC=["./manifest.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
