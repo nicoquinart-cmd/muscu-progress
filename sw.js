af8757bdbc907f0ad6828529d03d8bc015a79b47
nicoquinart-cmd <nicoquinart@gmail.com>
2026-08-31 22:11:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fitpilot-v5-1-1";
+const CACHE="fitpilot-v5-2";
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET") return;
   const url=new URL(event.request.url);
